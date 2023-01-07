6408f26535b3b98f5d144a5b7c6bcf25316a5010
Old_Man_Dapp <44881721+pmcf69@users.noreply.github.com>
2023-01-07 22:24:19 +0000

    Add files via upload

---
+++ pages/api/generate.js
@@ -0,0 +1,32 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`
+Write an answer, by describing in terms that an 18 year old revising for a college psychology exam, would understand on the topic below. Please make sure the answer goes in-depth on the topic and shows that it has been researched well. Make it feel like a story and explain why.
+
+Topic:
+`
+;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}\n`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.7,
+    max_tokens: 550,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
